a2c4cdf55028cb46b2b4c9b2d2a3779b21928fbd
FranciscoGontijo <franciscoacmg@gmail.com>
2024-04-03 10:21:59 -0300

    Fixing icon button css

--- src/components/icon-button.tsx
+++ src/components/icon-button.tsx
@@ -5,16 +5,13 @@ interface IconButtonProps extends ComponentProps<'button'> {
     transparent?: boolean;
 };
 
-const chevronIconStyle = "bg-white/10 border border-white/10 rounded-md p-1.5";
-const moreHorizontalStyle = "bg-black/20 border border-white/10 rounded-md p-1.5";
-
 export const IconButton = ({ transparent, ...props }: IconButtonProps) => {
     return (
         <button
             {...props}
             className={twMerge(
                 'border border-white/10 rounded-md p-1.5',
-                transparent ? 'bg-white/10' : 'bg-black/20',
+                transparent ? 'bg-black/20' : 'bg-white/10',
                 props.disabled && 'opacity-50'
             )
             } children={props.children} />
